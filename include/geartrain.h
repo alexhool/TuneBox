3198e2d86ab4762ce19b9e26454528d8af49328f
ahool3 <alexhool@gatech.edu>
2025-03-06 13:46:20 -0500

    remove geartrain parts

--- include/geartrain.h
+++
@@ -1,9 +0,0 @@
-#ifndef GEARTRAIN_H
-#define GEARTRAIN_H
-
-namespace geartrain {
-  void setup();
-  int example3(int x, float y, bool z); // Example function declaration
-}
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include <Arduino.h>
 #include "launchpad.h"
 #include "drummer.h"
-#include "geartrain.h"
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Initializing...");
   launchpad::setup();
   drummer::setup();
-  geartrain::setup();
 }
 
 void loop() {
