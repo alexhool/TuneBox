fbee5e950d524020b41bcb8a53fb99f18326e5a3
ahool3 <alexhool@gatech.edu>
2025-03-06 16:49:37 -0500

    implement tof based servo movement

--- include/drummer.h
+++ include/drummer.h
@@ -1,11 +1,11 @@
 #ifndef DRUMMER_H
 #define DRUMMER_H
 
-#include <Adafruit_VL53L0X.h>
+#include <VL53L0X.h>
 #include <ESP32Servo.h>
 
 namespace drummer {
-  extern Adafruit_VL53L0X lox;
+  extern VL53L0X tof;
   extern Servo servo;
   void setup();
   void start();
--- src/drummer.cpp
+++ src/drummer.cpp
@@ -1,25 +1,56 @@
 #include "drummer.h"
 #include <Arduino.h>
 
-Adafruit_VL53L0X drummer::lox = Adafruit_VL53L0X();
+VL53L0X drummer::tof;
 Servo drummer::servo;
 
-void drummer::setup() {
-  Serial.print("Initializing Drummer...");
-  // Initialize ToF
-  if (!lox.begin()) {
-    Serial.println("Failed to boot ToF");
-    while(1);
-  }
-  lox.startRangeContinuous();
+const int SERVO_PIN = 13;
+const int SERVO_START_POS = 90;
+const int SERVO_END_POS = 180;
+const int DISTANCE_THRESHOLD = 80;
 
-  Serial.println("Finished");
+void drummer::setup() {
+  Serial.println("|--Drummer Setup--|");
+
+  // Initialize Servo
+  Serial.print("Initializing Servo...");
+  ESP32PWM::timerCount[0] = 4;
+  ESP32PWM::allocateTimer(1);
+  servo.setPeriodHertz(50);
+  servo.attach(SERVO_PIN, 1000, 2000);
+  servo.write(SERVO_START_POS);
+  Serial.println("Done");
+
+  // Initialize ToF
+  Serial.print("Initializing ToF...");
+  Wire.begin(6, 7);
+  tof.setTimeout(100); // gives up after 100ms
+  if (!tof.init()) {
+    Serial.println("Failed to boot ToF");
+    while (1);
+  }
+  tof.setMeasurementTimingBudget(20000); // 20ms measurement time (default: 33ms)
+  tof.startContinuous();
+  Serial.println("Done");
+
+  Serial.println("|-----------------|");
 }
 
 void drummer::start() {
-  if (lox.isRangeComplete()) {
-    uint16_t distance = lox.readRange();
-    Serial.print("Distance in mm: ");
-    Serial.println(distance);
+  uint16_t distance = tof.readRangeContinuousMillimeters();
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if (distance < DISTANCE_THRESHOLD) {
+    Serial.println("Sweeping Servo");
+    for (int pos = SERVO_START_POS; pos <= SERVO_END_POS; pos++) {
+      servo.write(pos);
+      delay(15);
+    }
+    delay(500);
+    for (int pos = SERVO_END_POS; pos >= SERVO_START_POS; pos--) {
+      servo.write(pos);
+      delay(15);
+    }
   }
 }
